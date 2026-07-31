047ffb4b59d395c87d07f1550501422f247be27b
peps_win <210877492+peps-win@users.noreply.github.com>
2026-07-31 01:41:01 -0500

    Added some improvemnents to the ball like velocity and speed to adjust to position of cursor but it still loses the ball

--- src/main.rs
+++ src/main.rs
@@ -223,10 +223,21 @@ fn is_mouse_inside_ball(mouse_x: f32, mouse_y: f32, ball: &mut Ball) -> bool {
     (dx * dx + dy * dy) <= ball.radius*ball.radius
 }
 fn set_ball_to_mouse(mouse_x: f32, mouse_y: f32, ball: &mut Ball) {
-    ball.x_velocity = 0.0;
-    ball.y_velocity = 0.0;
-    ball.x = mouse_x;
-    ball.y = mouse_y;
+
+
+    //Changes the speed that the ball reacts 0.0 for no movement 1.0 for snapping
+    //Closer to 0.0 the looser it is and worse at latching on to mouse
+    let follow_speed: f32 = 0.9;
+
+    //Finds distance of ball to mouse
+    let dx = mouse_x - ball.x;
+    let dy = mouse_y - ball.y;
+
+    //Scale the velocity change by the distance which will support throwing of the balls
+    ball.x_velocity = dx * 0.3;
+    ball.y_velocity = dy * 0.3;
+    ball.x += (mouse_x - ball.x) * follow_speed;
+    ball.y += (mouse_y - ball.y) * follow_speed;
 }
 
 #[macroquad::main("Physics Engine")]
